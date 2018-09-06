efb1d42160ac2c50bc8989c5622805e4aa748c69
EmilsoSilva <EmilsoSilva@LAPTOP-8BNO6U21.nox5.lan>
2018-09-05 21:37:32 -0300

    meu primeiro commit dentro do netBeans

---
+++ src/alturapeso2/AlturaPeso2.java
@@ -0,0 +1,25 @@
+
+
+package alturapeso2;
+
+import java.util.Scanner;
+
+public class AlturaPeso2 {
+
+    public static void main(String[] args) {
+        float altura,peso;
+        Scanner leia = new Scanner(System.in);
+        System.out.println("Digite a Altura:1");
+        altura = leia.nextFloat();
+        System.out.println("Digite o Peso");
+        peso = leia.nextFloat();
+        if(altura<1.20){
+        if(peso<=60){
+            System.out.println("A classificação é A:");
+        
+            
+        }
+    }
+    }
+    
+}
